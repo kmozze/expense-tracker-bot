b7d3daeeb5ffc2b0b312b701765b36d0cd38c91a
Ekaterina Mozzhevilova <kdedovich@yandex.ru>
2026-05-22 19:32:02 +0300

    test: add adapter input contract tests

--- src/test/kotlin/me/kmozze/expensetracker/support/UserInputTestSupport.kt
+++ src/test/kotlin/me/kmozze/expensetracker/support/UserInputTestSupport.kt
@@ -3,6 +3,7 @@ package me.kmozze.expensetracker.support
 import me.kmozze.expensetracker.adapter.input.UserCommandParser
 import me.kmozze.expensetracker.handler.DialogueRouter
 import me.kmozze.expensetracker.model.domain.HandlerResult
+import me.kmozze.expensetracker.model.domain.UserCommand
 import me.kmozze.expensetracker.model.domain.UserInput
 
 fun makeUserInput(
@@ -10,13 +11,14 @@ fun makeUserInput(
     chatId: Long,
     text: String? = null,
     callbackData: String? = null,
+    command: UserCommand = UserCommandParser.parse(text = text, callbackData = callbackData),
 ): UserInput =
     UserInput(
         userId = userId,
         chatId = chatId,
         text = text,
         callbackData = callbackData,
-        command = UserCommandParser.parse(text = text, callbackData = callbackData),
+        command = command,
     )
 
 fun DialogueRouter.processUserInput(
@@ -24,4 +26,14 @@ fun DialogueRouter.processUserInput(
     chatId: Long,
     text: String? = null,
     callbackData: String? = null,
-): HandlerResult = process(makeUserInput(userId = userId, chatId = chatId, text = text, callbackData = callbackData))
+    command: UserCommand = UserCommandParser.parse(text = text, callbackData = callbackData),
+): HandlerResult =
+    process(
+        makeUserInput(
+            userId = userId,
+            chatId = chatId,
+            text = text,
+            callbackData = callbackData,
+            command = command,
+        ),
+    )
