c4628c37bee8f06e9f838a54214ede1fef0c1e57
Ekaterina Mozzhevilova <kdedovich@yandex.ru>
2026-05-26 19:32:16 +0300

    Use product timezone for expense quick dates

--- src/main/kotlin/me/kmozze/expensetracker/config/TimeConfig.kt
+++ src/main/kotlin/me/kmozze/expensetracker/config/TimeConfig.kt
@@ -3,9 +3,14 @@ package me.kmozze.expensetracker.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Clock
+import java.time.ZoneId
 
 @Configuration
 class TimeConfig {
     @Bean
-    fun clock(): Clock = Clock.systemDefaultZone()
+    fun clock(): Clock = Clock.system(EXPENSE_DATE_ZONE)
+
+    private companion object {
+        val EXPENSE_DATE_ZONE: ZoneId = ZoneId.of("Europe/Moscow")
+    }
 }
