04ab6ded05bddb99422d206aa337b2e7e445ca79
Ekaterina Mozzhevilova <kdedovich@yandex.ru>
2026-05-29 19:30:51 +0300

    feat(edit-expense): add edit-state contracts

--- src/main/kotlin/me/kmozze/expensetracker/model/domain/UserState.kt
+++ src/main/kotlin/me/kmozze/expensetracker/model/domain/UserState.kt
@@ -1,5 +1,7 @@
 package me.kmozze.expensetracker.model.domain
 
+import java.util.UUID
+
 sealed class UserState {
     data object Idle : UserState()
 
@@ -17,5 +19,29 @@ sealed class UserState {
     data class AwaitingExpenseManualDateInput(
         val expenseDraft: ExpenseDraft,
         val categoryName: String,
+    ) : UserState()
+
+    data class AwaitingExpenseEditFieldSelection(
+        val expenseId: UUID,
+    ) : UserState()
+
+    data class AwaitingExpenseAmountEdit(
+        val expenseId: UUID,
+    ) : UserState()
+
+    data class AwaitingExpenseCategoryEdit(
+        val expenseId: UUID,
+    ) : UserState()
+
+    data class AwaitingExpenseDateEditSelection(
+        val expenseId: UUID,
+    ) : UserState()
+
+    data class AwaitingExpenseDateEditManualInput(
+        val expenseId: UUID,
+    ) : UserState()
+
+    data class AwaitingExpenseDescriptionEdit(
+        val expenseId: UUID,
     ) : UserState()
 }
